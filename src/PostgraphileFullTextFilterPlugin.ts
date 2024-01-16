54e5cdc94dd0ed8a3df8cff25384f3656191176c
Roland Kiraly <roland.kiraly@pephealth.ai>
2024-01-16 11:27:00 +0000

    fix: update types

--- src/PostgraphileFullTextFilterPlugin.ts
+++ src/PostgraphileFullTextFilterPlugin.ts
@@ -1,17 +1,10 @@
 const { Tsquery } = require("pg-tsquery");
 
+import type { Build, Inflection, Plugin } from "graphile-build";
 import { omit } from "graphile-build-pg";
-import { Build, Inflection } from "graphile-build";
-import {
-  GraphQLObjectType,
-  GraphQLEnumType,
-  GraphQLScalarType,
-  GraphQLString,
-} from "graphql";
-import { Context } from "postgraphile-core";
 
 const tsquery = new Tsquery();
-export default function PostGraphileFulltextFilterPlugin(builder: Build): void {
+const PostGraphileFulltextFilterPlugin: Plugin = (builder) => {
   builder.hook("inflection", (inflection: Inflection, build: Build) =>
     build.extend(inflection, {
       fullTextScalarTypeName(): string {
@@ -36,11 +29,13 @@ export default function PostGraphileFulltextFilterPlugin(builder: Build): void {
     })
   );
 
-  builder.hook("build", (build: Build) => {
+  builder.hook("build", (build) => {
     const {
       pgIntrospectionResultsByKind: introspectionResultsByKind,
       pgRegisterGqlTypeByTypeId: registerGqlTypeByTypeId,
       pgRegisterGqlInputTypeByTypeId: registerGqlInputTypeByTypeId,
+      graphql: { GraphQLScalarType },
+      inflection,
     } = build;
 
     const tsvectorType = introspectionResultsByKind.type.find(
@@ -50,7 +45,7 @@ export default function PostGraphileFulltextFilterPlugin(builder: Build): void {
       throw new Error("Unable to find tsvector type through introspection.");
     }
 
-    const scalarName = build.inflection.fullTextScalarTypeName();
+    const scalarName = inflection.fullTextScalarTypeName();
 
     const GraphQLFullTextType = new GraphQLScalarType({
       name: scalarName,
@@ -73,11 +68,12 @@ export default function PostGraphileFulltextFilterPlugin(builder: Build): void {
     });
   });
 
-  builder.hook("init", (_: any, build: Build) => {
+  builder.hook("build", (_, build) => {
     const {
       addConnectionFilterOperator,
       pgSql: sql,
       pgGetGqlInputTypeByTypeIdAndModifier: getGqlInputTypeByTypeIdAndModifier,
+      graphql: { GraphQLString },
       pgTsvType,
     } = build;
 
@@ -113,231 +109,223 @@ export default function PostGraphileFulltextFilterPlugin(builder: Build): void {
     return build;
   });
 
-  builder.hook(
-    "GraphQLObjectType:fields",
-    (fields: GraphQLObjectType, build: Build, context) => {
-      const {
-        pgIntrospectionResultsByKind: introspectionResultsByKind,
-        graphql: { GraphQLFloat },
-        pgColumnFilter,
-        pg2gql,
-        pgSql: sql,
-        inflection,
-        pgTsvType,
-      } = build;
+  builder.hook("GraphQLObjectType:fields", (fields, build, context) => {
+    const {
+      pgIntrospectionResultsByKind: introspectionResultsByKind,
+      graphql: { GraphQLFloat },
+      pgColumnFilter,
+      pg2gql,
+      pgSql: sql,
+      inflection,
+      pgTsvType,
+    } = build;
 
-      const {
-        scope: { isPgRowType, isPgCompoundType, pgIntrospection: table },
-        fieldWithHooks,
-      } = context;
+    const {
+      scope: { isPgRowType, isPgCompoundType, pgIntrospection: table },
+      fieldWithHooks,
+    } = context;
 
-      if (
-        !(isPgRowType || isPgCompoundType) ||
-        !table ||
-        table.kind !== "class" ||
-        !pgTsvType
-      ) {
-        return fields;
-      }
-
-      const tableType = introspectionResultsByKind.type.filter(
-        (type: { type: string; namespaceId: any; classId: any }) =>
-          type.type === "c" &&
-          type.namespaceId === table.namespaceId &&
-          type.classId === table.id
-      )[0];
-      if (!tableType) {
-        throw new Error("Could not determine the type of this table.");
-      }
-
-      const tsvColumns = table.attributes
-        .filter((attr) => attr.typeId === pgTsvType.id)
-        .filter((attr) => pgColumnFilter(attr, build, context))
-        .filter((attr) => !omit(attr, "filter"));
-
-      const tsvProcs = introspectionResultsByKind.procedure
-        .filter((proc) => proc.isStable)
-        .filter((proc) => proc.namespaceId === table.namespaceId)
-        .filter((proc) => proc.name.startsWith(`${table.name}_`))
-        .filter((proc) => proc.argTypeIds.length > 0)
-        .filter((proc) => proc.argTypeIds[0] === tableType.id)
-        .filter((proc) => proc.returnTypeId === pgTsvType.id)
-        .filter((proc) => !omit(proc, "filter"));
-
-      if (tsvColumns.length === 0 && tsvProcs.length === 0) {
-        return fields;
-      }
-
-      const newRankField = (baseFieldName, rankFieldName) =>
-        fieldWithHooks(
-          rankFieldName,
-          ({ addDataGenerator }) => {
-            addDataGenerator(({ alias }) => ({
-              pgQuery: (queryBuilder) => {
-                const { parentQueryBuilder } = queryBuilder;
-                if (
-                  !parentQueryBuilder ||
-                  !parentQueryBuilder.__fts_ranks ||
-                  !parentQueryBuilder.__fts_ranks[baseFieldName]
-                ) {
-                  return;
-                }
-                const [identifier, tsQueryString] =
-                  parentQueryBuilder.__fts_ranks[baseFieldName];
-                queryBuilder.select(
-                  sql.fragment`ts_rank(${identifier}, to_tsquery(${sql.value(
-                    tsQueryString
-                  )}))`,
-                  alias
-                );
-              },
-            }));
-            return {
-              description: `Full-text search ranking when filtered by \`${baseFieldName}\`.`,
-              type: GraphQLFloat,
-              resolve: (data) => pg2gql(data[rankFieldName], GraphQLFloat),
-            };
-          },
-          {
-            isPgTSVRankField: true,
-          }
-        );
-
-      const tsvFields = tsvColumns.reduce((memo, attr) => {
-        const fieldName = inflection.column(attr);
-        const rankFieldName = inflection.pgTsvRank(fieldName);
-        memo[rankFieldName] = newRankField(fieldName, rankFieldName); // eslint-disable-line no-param-reassign
-
-        return memo;
-      }, {});
-
-      const tsvProcFields = tsvProcs.reduce((memo, proc) => {
-        const psuedoColumnName = proc.name.substr(table.name.length + 1);
-        const fieldName = inflection.computedColumn(
-          psuedoColumnName,
-          proc,
-          table
-        );
-        const rankFieldName = inflection.pgTsvRank(fieldName);
-        memo[rankFieldName] = newRankField(fieldName, rankFieldName); // eslint-disable-line no-param-reassign
-
-        return memo;
-      }, {});
-
-      return { ...fields, ...tsvFields, ...tsvProcFields };
+    if (
+      !(isPgRowType || isPgCompoundType) ||
+      !table ||
+      table.kind !== "class" ||
+      !pgTsvType
+    ) {
+      return fields;
     }
-  );
 
-  builder.hook(
-    "GraphQLEnumType:values",
-    (
-      values: GraphQLEnumType,
-      build: Build,
-      context: Context<GraphQLObjectType>
-    ) => {
-      const {
-        extend,
-        pgSql: sql,
-        pgColumnFilter,
-        pgIntrospectionResultsByKind: introspectionResultsByKind,
-        inflection,
-        pgTsvType,
-      } = build;
+    const tableType = introspectionResultsByKind.type.filter(
+      (type: { type: string; namespaceId: any; classId: any }) =>
+        type.type === "c" &&
+        type.namespaceId === table.namespaceId &&
+        type.classId === table.id
+    )[0];
+    if (!tableType) {
+      throw new Error("Could not determine the type of this table.");
+    }
 
-      const {
-        scope: { isPgRowSortEnum, pgIntrospection: table },
-      } = context;
+    const tsvColumns = table.attributes
+      .filter((attr: { typeId: any }) => attr.typeId === pgTsvType.id)
+      .filter((attr) => pgColumnFilter(attr, build, context))
+      .filter((attr) => !omit(attr, "filter"));
 
-      if (!isPgRowSortEnum || !table || table.kind !== "class" || !pgTsvType) {
-        return values;
-      }
+    const tsvProcs = introspectionResultsByKind.procedure
+      .filter((proc) => proc.isStable)
+      .filter((proc) => proc.namespaceId === table.namespaceId)
+      .filter((proc) => proc.name.startsWith(`${table.name}_`))
+      .filter((proc) => proc.argTypeIds.length > 0)
+      .filter((proc) => proc.argTypeIds[0] === tableType.id)
+      .filter((proc) => proc.returnTypeId === pgTsvType.id)
+      .filter((proc) => !omit(proc, "filter"));
 
-      const tableType = introspectionResultsByKind.type.filter(
-        (type) =>
-          type.type === "c" &&
-          type.namespaceId === table.namespaceId &&
-          type.classId === table.id
-      )[0];
-      if (!tableType) {
-        throw new Error("Could not determine the type of this table.");
-      }
+    if (tsvColumns.length === 0 && tsvProcs.length === 0) {
+      return fields;
+    }
 
-      const tsvColumns = introspectionResultsByKind.attribute
-        .filter((attr) => attr.classId === table.id)
-        .filter((attr) => attr.typeId === pgTsvType.id);
-
-      const tsvProcs = introspectionResultsByKind.procedure
-        .filter((proc) => proc.isStable)
-        .filter((proc) => proc.namespaceId === table.namespaceId)
-        .filter((proc) => proc.name.startsWith(`${table.name}_`))
-        .filter((proc) => proc.argTypeIds.length === 1)
-        .filter((proc) => proc.argTypeIds[0] === tableType.id)
-        .filter((proc) => proc.returnTypeId === pgTsvType.id)
-        .filter((proc) => !omit(proc, "order"));
-
-      if (tsvColumns.length === 0 && tsvProcs.length === 0) {
-        return values;
-      }
-
-      return extend(
-        values,
-        tsvColumns
-          .concat(tsvProcs)
-          .filter((attr) => pgColumnFilter(attr, build, context))
-          .filter((attr) => !omit(attr, "order"))
-          .reduce((memo, attr) => {
-            const fieldName =
-              attr.kind === "procedure"
-                ? inflection.computedColumn(
-                    attr.name.substr(table.name.length + 1),
-                    attr,
-                    table
-                  )
-                : inflection.column(attr);
-            const ascFieldName = inflection.pgTsvOrderByColumnRankEnum(
-              table,
-              attr,
-              true
-            );
-            const descFieldName = inflection.pgTsvOrderByColumnRankEnum(
-              table,
-              attr,
-              false
-            );
-
-            const findExpr = ({ queryBuilder }) => {
+    const newRankField = (baseFieldName, rankFieldName) =>
+      fieldWithHooks(
+        rankFieldName,
+        ({ addDataGenerator }) => {
+          addDataGenerator(({ alias }) => ({
+            pgQuery: (queryBuilder) => {
+              const { parentQueryBuilder } = queryBuilder;
               if (
-                !queryBuilder.__fts_ranks ||
-                !queryBuilder.__fts_ranks[fieldName]
+                !parentQueryBuilder ||
+                !parentQueryBuilder.__fts_ranks ||
+                !parentQueryBuilder.__fts_ranks[baseFieldName]
               ) {
-                return sql.fragment`1`;
+                return;
               }
               const [identifier, tsQueryString] =
-                queryBuilder.__fts_ranks[fieldName];
-              return sql.fragment`ts_rank(${identifier}, to_tsquery(${sql.value(
-                tsQueryString
-              )}))`;
-            };
-
-            memo[ascFieldName] = {
-              // eslint-disable-line no-param-reassign
-              value: {
-                alias: `${ascFieldName.toLowerCase()}`,
-                specs: [[findExpr, true]],
-              },
-            };
-            memo[descFieldName] = {
-              // eslint-disable-line no-param-reassign
-              value: {
-                alias: `${descFieldName.toLowerCase()}`,
-                specs: [[findExpr, false]],
-              },
-            };
-
-            return memo;
-          }, {}),
-        `Adding TSV rank columns for sorting on table '${table.name}'`
+                parentQueryBuilder.__fts_ranks[baseFieldName];
+              queryBuilder.select(
+                sql.fragment`ts_rank(${identifier}, to_tsquery(${sql.value(
+                  tsQueryString
+                )}))`,
+                alias
+              );
+            },
+          }));
+          return {
+            description: `Full-text search ranking when filtered by \`${baseFieldName}\`.`,
+            type: GraphQLFloat,
+            resolve: (data) => pg2gql(data[rankFieldName], GraphQLFloat),
+          };
+        },
+        {
+          isPgTSVRankField: true,
+        }
       );
+
+    const tsvFields = tsvColumns.reduce((memo, attr) => {
+      const fieldName = inflection.column(attr);
+      const rankFieldName = inflection.pgTsvRank(fieldName);
+      memo[rankFieldName] = newRankField(fieldName, rankFieldName); // eslint-disable-line no-param-reassign
+
+      return memo;
+    }, {});
+
+    const tsvProcFields = tsvProcs.reduce((memo, proc) => {
+      const psuedoColumnName = proc.name.substr(table.name.length + 1);
+      const fieldName = inflection.computedColumn(
+        psuedoColumnName,
+        proc,
+        table
+      );
+      const rankFieldName = inflection.pgTsvRank(fieldName);
+      memo[rankFieldName] = newRankField(fieldName, rankFieldName); // eslint-disable-line no-param-reassign
+
+      return memo;
+    }, {});
+
+    return { ...fields, ...tsvFields, ...tsvProcFields };
+  });
+
+  builder.hook("GraphQLEnumType:values", (values, build, context) => {
+    const {
+      extend,
+      pgSql: sql,
+      pgColumnFilter,
+      pgIntrospectionResultsByKind: introspectionResultsByKind,
+      inflection,
+      pgTsvType,
+    } = build;
+
+    const {
+      scope: { isPgRowSortEnum, pgIntrospection: table },
+    } = context;
+
+    if (!isPgRowSortEnum || !table || table.kind !== "class" || !pgTsvType) {
+      return values;
     }
-  );
-}
+
+    const tableType = introspectionResultsByKind.type.filter(
+      (type) =>
+        type.type === "c" &&
+        type.namespaceId === table.namespaceId &&
+        type.classId === table.id
+    )[0];
+    if (!tableType) {
+      throw new Error("Could not determine the type of this table.");
+    }
+
+    const tsvColumns = introspectionResultsByKind.attribute
+      .filter((attr) => attr.classId === table.id)
+      .filter((attr) => attr.typeId === pgTsvType.id);
+
+    const tsvProcs = introspectionResultsByKind.procedure
+      .filter((proc) => proc.isStable)
+      .filter((proc) => proc.namespaceId === table.namespaceId)
+      .filter((proc) => proc.name.startsWith(`${table.name}_`))
+      .filter((proc) => proc.argTypeIds.length === 1)
+      .filter((proc) => proc.argTypeIds[0] === tableType.id)
+      .filter((proc) => proc.returnTypeId === pgTsvType.id)
+      .filter((proc) => !omit(proc, "order"));
+
+    if (tsvColumns.length === 0 && tsvProcs.length === 0) {
+      return values;
+    }
+
+    return extend(
+      values,
+      tsvColumns
+        .concat(tsvProcs)
+        .filter((attr) => pgColumnFilter(attr, build, context))
+        .filter((attr) => !omit(attr, "order"))
+        .reduce((memo, attr) => {
+          const fieldName =
+            attr.kind === "procedure"
+              ? inflection.computedColumn(
+                  attr.name.substr(table.name.length + 1),
+                  attr,
+                  table
+                )
+              : inflection.column(attr);
+          const ascFieldName = inflection.pgTsvOrderByColumnRankEnum(
+            table,
+            attr,
+            true
+          );
+          const descFieldName = inflection.pgTsvOrderByColumnRankEnum(
+            table,
+            attr,
+            false
+          );
+
+          const findExpr = ({ queryBuilder }) => {
+            if (
+              !queryBuilder.__fts_ranks ||
+              !queryBuilder.__fts_ranks[fieldName]
+            ) {
+              return sql.fragment`1`;
+            }
+            const [identifier, tsQueryString] =
+              queryBuilder.__fts_ranks[fieldName];
+            return sql.fragment`ts_rank(${identifier}, to_tsquery(${sql.value(
+              tsQueryString
+            )}))`;
+          };
+
+          memo[ascFieldName] = {
+            // eslint-disable-line no-param-reassign
+            value: {
+              alias: `${ascFieldName.toLowerCase()}`,
+              specs: [[findExpr, true]],
+            },
+          };
+          memo[descFieldName] = {
+            // eslint-disable-line no-param-reassign
+            value: {
+              alias: `${descFieldName.toLowerCase()}`,
+              specs: [[findExpr, false]],
+            },
+          };
+
+          return memo;
+        }, {}),
+      `Adding TSV rank columns for sorting on table '${table.name}'`
+    );
+  });
+};
+
+export default PostGraphileFulltextFilterPlugin;
